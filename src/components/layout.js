75acfb411021925d7fba3e73b9992599df28518d
sthomp <sthompson553@gmail.com>
2021-01-03 20:39:43 -0500

    className

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 export default function Layout({ children }) {
   return (
     <main>
-      <div class="flex flex-row justify-end space-x-4">
+      <div className="flex flex-row justify-end space-x-4">
         <Link to="/">House</Link>
         <Link to="/inspiration">Inpsiration</Link>
       </div>
