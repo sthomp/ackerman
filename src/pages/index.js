b634f7bb752d76de96e88ec285d19107338d5e72
sthomp <sthompson553@gmail.com>
2021-01-03 19:34:16 -0500

    more pics

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,62 @@ const IndexPage = () => {
           }
         }
       }
+
+      garage: file(relativePath: { eq: "garage.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      basement1: file(relativePath: { eq: "house_basement1.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      basement2: file(relativePath: { eq: "house_basement2.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      front: file(relativePath: { eq: "house_front.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      rear: file(relativePath: { eq: "house_rear.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      street: file(relativePath: { eq: "house_street.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      top_floor: file(relativePath: { eq: "top_floor.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }
   `);
   return (
@@ -28,6 +84,13 @@ const IndexPage = () => {
       <title>Home Page</title>
       <Img fluid={images.survey.childImageSharp.fluid} alt="Land Survey" />
       <Img fluid={images.beam.childImageSharp.fluid} alt="New Beam" />
+      <Img fluid={images.garage.childImageSharp.fluid} alt="Garage" />
+      <Img fluid={images.basement1.childImageSharp.fluid} alt="Basement1" />
+      <Img fluid={images.basement2.childImageSharp.fluid} alt="Basement2" />
+      <Img fluid={images.front.childImageSharp.fluid} alt="Front" />
+      <Img fluid={images.rear.childImageSharp.fluid} alt="Rear" />
+      <Img fluid={images.street.childImageSharp.fluid} alt="Street" />
+      <Img fluid={images.top_floor.childImageSharp.fluid} alt="Top Floor" />
       <iframe
         width="560"
         height="315"
--- src/pages/inspiration.js
+++ src/pages/inspiration.js
@@ -5,13 +5,40 @@ import { useStaticQuery, graphql } from 'gatsby';
 const InspirationPage = () => {
   const images = useStaticQuery(graphql`
     query InspirationQuery {
-      zillow1: remoteImagesYaml(name: { eq: "Zillow Image1" }) {
-        name
+      _27_ackerman_exterior: remoteImagesYaml(
+        id: { eq: "27_ackerman_exterior" }
+      ) {
         localImage {
           childImageSharp {
             fluid {
               ...GatsbyImageSharpFluid
             }
+          }
+        }
+      }
+
+      _6_spring_interior: remoteImagesYaml(id: { eq: "6_spring_interior" }) {
+        localImage {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+
+      half_bath1: file(relativePath: { eq: "inspiration_half_bath1.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      half_bath2: file(relativePath: { eq: "inspiration_half_bath2.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
           }
         }
       }
@@ -21,9 +48,15 @@ const InspirationPage = () => {
     <main>
       <p>Inspiration</p>
       <Img
-        fluid={images.zillow1.localImage.childImageSharp.fluid}
+        fluid={images._6_spring_interior.localImage.childImageSharp.fluid}
+        alt="6 Spring Interior"
+      />
+      <Img
+        fluid={images._27_ackerman_exterior.localImage.childImageSharp.fluid}
         alt="27 Ackerman"
       />
+      <Img fluid={images.half_bath1.childImageSharp.fluid} alt="Half Bath1" />
+      <Img fluid={images.half_bath2.childImageSharp.fluid} alt="Half Bath2" />
     </main>
   );
 };
