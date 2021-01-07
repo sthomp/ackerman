ffa2b1831fb6bf5f87e7d5623d7cfa63de5de760
sthomp <sthompson553@gmail.com>
2021-01-06 23:09:09 -0500

    cleanup galleryImage

--- src/components/GalleryImage.js
+++ src/components/GalleryImage.js
@@ -6,14 +6,12 @@ export default function GalleryImage({ data }) {
   // RemoteImages will use `localImage` whereas local images will use `image`
   const imageData = data.localImage || data.image;
   return (
-    <div className='relative'>
-      <div>
-        <Img
-          fluid={imageData.childImageSharp.fluid}
-          alt={data.desc}
-          style={{ height: '100%' }}
-        />
-      </div>
+    <div className='relative h-full'>
+      <Img
+        fluid={imageData.childImageSharp.fluid}
+        alt={data.desc}
+        style={{ height: '100%' }}
+      />
       {data.desc && (
         <div className='opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-opacity-50 absolute left-0 top-0 right-0 bottom-0 w-full bg-black flex justify-center items-center p-2'>
           {<Heading3 light={true}>{data.desc}</Heading3>}
