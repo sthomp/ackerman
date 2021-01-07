66da35d0a5742249fcb978aa97f8a980db5bfe84
sthomp <sthompson553@gmail.com>
2021-01-06 23:00:36 -0500

    remove some descriptions

--- src/components/GalleryImage.js
+++ src/components/GalleryImage.js
@@ -14,9 +14,11 @@ export default function GalleryImage({ data }) {
           style={{ height: '100%' }}
         />
       </div>
-      <div className='opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-opacity-50 absolute left-0 top-0 right-0 bottom-0 w-full bg-black flex justify-center items-center p-2'>
-        {<Heading3 light={true}>{data.desc}</Heading3>}
-      </div>
+      {data.desc && (
+        <div className='opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-opacity-50 absolute left-0 top-0 right-0 bottom-0 w-full bg-black flex justify-center items-center p-2'>
+          {<Heading3 light={true}>{data.desc}</Heading3>}
+        </div>
+      )}
     </div>
   );
 }
